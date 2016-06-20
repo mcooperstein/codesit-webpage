86c81b705f222f6b4fba2330d0515298a6333f16
Marc Cooperstein <mcooperstein@gmail.com>
2016-06-19 17:10:49 -0700

    still trying to get email form working
    still trying to get email form working

--- js/email.js
+++ js/email.js
@@ -5,17 +5,20 @@ $(document).ready(function () {
         var data = {},
             contactName = $('#contactName').val(),
             contactMessage = $('#contactMessage').val(),
-            contactEmail = $('#contactEmail').val();
+            contactEmail = $('#contactEmail').val(),
+            //added contactPhone variable and data
+            contactPhone = $('#contactPhone').val();
 
-        if (Validate(contactName) && Validate(contactMessage) && Validate(contactEmail)) {
+        if (Validate(contactName) && Validate(contactMessage) && Validate(contactEmail) && Validate(contactPhone)) {
 
             data.contactName = contactName;
             data.contactEmail = contactEmail;
             data.contactMessage = contactMessage;
+            data.contactPhone = contactPhone;
 
             jQuery.ajax({
                 type: "POST",
-                url: "../mail/email.php",
+                url: "././mail/email.php",
                 data: data,
                 success: function (data) {
                     document.getElementById("contactForm").reset();
